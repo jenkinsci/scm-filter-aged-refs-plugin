e6053eed397e90ffe26c9a14bf0c3e7ba8a65910
witokondoria <witokondoria@gmail.com>
2017-09-25 12:36:56 +0200

    including forgotten renamed modules

---
+++ bitbucket-source-aged-refs-trait-plugin/src/main/java/org/jenkinsci/plugins/bitbucket_source_aged/AgedRefsStrategyTrait.java
@@ -0,0 +1,101 @@
+package org.jenkinsci.plugins.bitbucket_source_aged;
+
+import com.cloudbees.jenkins.plugins.bitbucket.BitbucketGitSCMBuilder;
+import com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMSourceRequest;
+import com.cloudbees.jenkins.plugins.bitbucket.BranchSCMHead;
+import com.cloudbees.jenkins.plugins.bitbucket.PullRequestSCMHead;
+import com.cloudbees.jenkins.plugins.bitbucket.api.BitbucketBranch;
+import com.cloudbees.jenkins.plugins.bitbucket.api.BitbucketPullRequest;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import jenkins.scm.api.SCMHead;
+import jenkins.scm.api.trait.SCMBuilder;
+import jenkins.scm.api.trait.SCMSourceContext;
+import jenkins.scm.api.trait.SCMSourceRequest;
+import org.jenkinsci.plugins.branch_source_aged.AgedStrategyTrait;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * @author witokondoria
+ */
+public class AgedRefsStrategyTrait extends AgedStrategyTrait{
+
+    /**
+     * Constructor for stapler.
+     *
+     * @param retentionDays
+     */
+    public AgedRefsStrategyTrait(String retentionDays) {
+        super(retentionDays);
+    }
+
+    @Override
+    protected void decorateContext(SCMSourceContext<?, ?> context) {
+        if (this.retentionDays > 0) {
+            context.withFilter(new AgedRefsStrategyTrait.ExcludeOldBranchesSCMHeadFilter(this.retentionDays));
+        }
+    }
+
+    /**
+     * Our descriptor.
+     */
+    @Extension
+    @SuppressWarnings("unused") // instantiated by Jenkins
+    public static class DescriptorImpl extends AgedRefsDescriptorImpl {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String getDisplayName() {
+            return super.getDisplayName();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean isApplicableToBuilder(@NonNull Class<? extends SCMBuilder> builderClass) {
+            return BitbucketGitSCMBuilder.class.isAssignableFrom(builderClass);
+        }
+
+    }
+
+    /**
+     * Filter that excludes references (branches or pull requests) according to its last commit modification date and the defined retentionDays.
+     */
+    public static class ExcludeOldBranchesSCMHeadFilter extends ExcludeBranchesSCMHeadFilter {
+
+        public ExcludeOldBranchesSCMHeadFilter(int retentionDays) {
+            super(retentionDays);
+        }
+
+        @Override
+        public boolean isExcluded(@NonNull SCMSourceRequest scmSourceRequest, @NonNull SCMHead scmHead) throws IOException, InterruptedException {
+            if (scmHead instanceof BranchSCMHead) {
+                Iterable<BitbucketBranch> branches = ((BitbucketSCMSourceRequest) scmSourceRequest).getBranches();
+                Iterator<BitbucketBranch> branchIterator = branches.iterator();
+                while (branchIterator.hasNext()) {
+                    BitbucketBranch branch = branchIterator.next();
+                    long branchTS = branch.getDateMillis();
+                    if (branch.getName().equals(scmHead.getName())) {
+                        return (Long.compare(branchTS, super.getAcceptableDateTimeThreshold()) < 0);
+                    }
+                }
+            } else if (scmHead instanceof PullRequestSCMHead) {
+                Iterable<BitbucketPullRequest> pulls = ((BitbucketSCMSourceRequest) scmSourceRequest).getPullRequests();
+                Iterator<BitbucketPullRequest> pullIterator = pulls.iterator();
+                while (pullIterator.hasNext()) {
+                    BitbucketPullRequest pull = pullIterator.next();
+                    if (pull.getSource().getBranch().getName().equals(scmHead.getName())) {
+                        long pullTS = pull.getSource().getCommit().getDateMillis();
+                        return (Long.compare(pullTS, super.getAcceptableDateTimeThreshold()) < 0);
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
