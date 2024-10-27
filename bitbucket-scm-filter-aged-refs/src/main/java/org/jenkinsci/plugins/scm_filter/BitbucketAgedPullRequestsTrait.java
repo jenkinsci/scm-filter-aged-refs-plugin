a04988df0d561a38481a4ea5647a5ef9d190972e
Jesus Lopez Pinzolas <jesus@trucosuso.com>
2024-10-27 21:25:20 +0100

    feat: add trait to exclude aged pull requests

---
+++ bitbucket-scm-filter-aged-refs/src/main/java/org/jenkinsci/plugins/scm_filter/BitbucketAgedPullRequestsTrait.java
@@ -0,0 +1,93 @@
+package org.jenkinsci.plugins.scm_filter;
+
+import com.cloudbees.jenkins.plugins.bitbucket.*;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import java.io.IOException;
+import jenkins.scm.api.SCMHead;
+import jenkins.scm.api.SCMSource;
+import jenkins.scm.api.trait.SCMSourceContext;
+import jenkins.scm.api.trait.SCMSourceRequest;
+import jenkins.scm.impl.trait.Selection;
+import org.jenkinsci.Symbol;
+import org.jenkinsci.plugins.scm_filter.enums.RefType;
+import org.jenkinsci.plugins.scm_filter.utils.FilterRefUtils;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * @author witokondoria
+ */
+public class BitbucketAgedPullRequestsTrait extends AgedTypeRefsTrait {
+    private static final RefType REF_TYPE = RefType.PULL_REQUEST;
+
+    /**
+     * Constructor for stapler.
+     *
+     * @param retentionDays retention period in days
+     */
+    @DataBoundConstructor
+    public BitbucketAgedPullRequestsTrait(String retentionDays) {
+        super(retentionDays);
+    }
+
+    @Override
+    protected void decorateContext(SCMSourceContext<?, ?> context) {
+        if (retentionDays > 0) {
+            context.withFilter(new ExcludeOldPullRequestsSCMHeadFilter(retentionDays));
+        }
+    }
+
+    /**
+     * Our descriptor.
+     */
+    @Extension
+    @Selection
+    @Symbol("bitbucketAgedPullRequestsTrait")
+    @SuppressWarnings("unused") // instantiated by Jenkins
+    public static class DescriptorImpl extends AgedRefsDescriptorImpl {
+
+        @Override
+        public Class<? extends SCMSourceContext> getContextClass() {
+            return BitbucketSCMSourceContext.class;
+        }
+
+        @Override
+        public Class<? extends SCMSource> getSourceClass() {
+            return BitbucketSCMSource.class;
+        }
+
+        @Override
+        @NonNull
+        public String getDisplayName() {
+            return "Filter pull requests by age";
+        }
+
+        @Override
+        @NonNull
+        public String getRefName() {
+            return REF_TYPE.getName();
+        }
+    }
+
+    /**
+     * Filter that excludes pull requests according to their last commit modification date and the defined retentionDays.
+     */
+    private static class ExcludeOldPullRequestsSCMHeadFilter extends ExcludeReferencesSCMHeadFilter {
+
+        ExcludeOldPullRequestsSCMHeadFilter(int retentionDays) {
+            super(retentionDays);
+        }
+
+        @Override
+        public boolean isExcluded(@NonNull SCMSourceRequest scmSourceRequest, @NonNull SCMHead scmHead)
+                throws IOException, InterruptedException {
+            if (scmHead instanceof PullRequestSCMHead) {
+                return FilterRefUtils.isPullRequestExcluded(
+                        (BitbucketSCMSourceRequest) scmSourceRequest,
+                        (PullRequestSCMHead) scmHead,
+                        super.getAcceptableDateTimeThreshold());
+            }
+            return false;
+        }
+    }
+}
---
+++ github-scm-filter-aged-refs/src/main/java/org/jenkinsci/plugins/scm_filter/GitHubAgedPullRequestsTrait.java
@@ -0,0 +1,93 @@
+package org.jenkinsci.plugins.scm_filter;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import java.io.IOException;
+import jenkins.scm.api.SCMHead;
+import jenkins.scm.api.SCMSource;
+import jenkins.scm.api.trait.SCMSourceContext;
+import jenkins.scm.api.trait.SCMSourceRequest;
+import jenkins.scm.impl.trait.Selection;
+import org.jenkinsci.Symbol;
+import org.jenkinsci.plugins.github_branch_source.*;
+import org.jenkinsci.plugins.scm_filter.enums.RefType;
+import org.jenkinsci.plugins.scm_filter.utils.FilterRefUtils;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * @author witokondoria
+ */
+public class GitHubAgedPullRequestsTrait extends AgedTypeRefsTrait {
+    private static final RefType REF_TYPE = RefType.PULL_REQUEST;
+
+    /**
+     * Constructor for stapler.
+     *
+     * @param retentionDays retention period in days
+     */
+    @DataBoundConstructor
+    public GitHubAgedPullRequestsTrait(String retentionDays) {
+        super(retentionDays);
+    }
+
+    @Override
+    protected void decorateContext(SCMSourceContext<?, ?> context) {
+        if (retentionDays > 0) {
+            context.withFilter(new ExcludeOldPullRequestsSCMHeadFilter(retentionDays));
+        }
+    }
+
+    /**
+     * Our descriptor.
+     */
+    @Extension
+    @Selection
+    @Symbol("gitHubAgedPullRequestsTrait")
+    @SuppressWarnings("unused") // instantiated by Jenkins
+    public static class DescriptorImpl extends AgedRefsDescriptorImpl {
+
+        @Override
+        public Class<? extends SCMSourceContext> getContextClass() {
+            return GitHubSCMSourceContext.class;
+        }
+
+        @Override
+        public Class<? extends SCMSource> getSourceClass() {
+            return GitHubSCMSource.class;
+        }
+
+        @Override
+        @NonNull
+        public String getDisplayName() {
+            return "Filter pull requests by age";
+        }
+
+        @Override
+        @NonNull
+        public String getRefName() {
+            return REF_TYPE.getName();
+        }
+    }
+
+    /**
+     * Filter that excludes pull requests according to their last commit modification date and the defined retentionDays.
+     */
+    private static class ExcludeOldPullRequestsSCMHeadFilter extends ExcludeReferencesSCMHeadFilter {
+
+        ExcludeOldPullRequestsSCMHeadFilter(int retentionDays) {
+            super(retentionDays);
+        }
+
+        @Override
+        public boolean isExcluded(@NonNull SCMSourceRequest scmSourceRequest, @NonNull SCMHead scmHead)
+                throws IOException, InterruptedException {
+            if (scmHead instanceof PullRequestSCMHead) {
+                return FilterRefUtils.isPullRequestExcluded(
+                        (GitHubSCMSourceRequest) scmSourceRequest,
+                        (PullRequestSCMHead) scmHead,
+                        super.getAcceptableDateTimeThreshold());
+            }
+            return false;
+        }
+    }
+}
